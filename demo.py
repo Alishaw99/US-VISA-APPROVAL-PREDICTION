e78ba73a33d3fb5949d13fba8acfb969b395312a
Alishaw99 <tariqham@gmail.com>
2024-03-06 14:46:36 -0800

    utills _added

--- demo.py
+++ demo.py
@@ -1,11 +1,23 @@
+from us_visa.logger import logging
+from us_visa.exception import USvisaException
+import sys
+
+
+
+try:
+    a = 1 / "10"
+    
+except Exception as e:    
+    raise USvisaException(e, sys) from e
+    
+    
+    
+
+
+
 # from us_visa.constants import DATABASE_NAME
 
 # print(DATABASE_NAME)
 
 # # pipline  = TrainPipeline()
 # # pipline.run_pipeline()
-
-
-from us_visa.logger import logging
-
-logging.info("Welcome to our custom log")
--- us_visa/logger/__init__.py
+++ us_visa/logger/__init__.py
@@ -1,18 +1,20 @@
 import logging
 import os
-
-from from_root import from_root
 from datetime import datetime
 
+# Define the log file name using the current timestamp
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-log_dir = 'logs'
+# Define the path of the log folder
+log_dir = r'D:\US-VISA-APPROVAL-PREDICTION\logs'
 
-logs_path = os.path.join(from_root(), log_dir, LOG_FILE)
+# Concatenate the log file path
+logs_path = os.path.join(log_dir, LOG_FILE)
 
+# Ensure that the log directory exists; create it if it doesn't
 os.makedirs(log_dir, exist_ok=True)
 
-
+# Configure logging
 logging.basicConfig(
     filename=logs_path,
     format="[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s",
