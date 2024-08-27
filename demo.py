a655e4d3c579307c0f932cb7dcc332d0bdfb5184
Alishaw99 <tariqham@gmail.com>
2024-08-26 17:39:26 -0700

    2-run1

--- demo.py
+++ demo.py
@@ -1,6 +1,12 @@
-from us_visa.pipline.training_pipeline import TrainPipeline
+#from us_visa.pipline.training_pipeline import TrainPipeline
 
 
 
-pipline  = TrainPipeline()
-pipline.run_pipeline()
+# pipline  = TrainPipeline()
+# pipline.run_pipeline()
+
+
+from us_visa.logger import logging
+
+logging.info("Welcome to our custom log")
+
